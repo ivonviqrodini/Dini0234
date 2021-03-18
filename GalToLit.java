401c62bf2974365af068cbee03896b718bb351b6
Ivon Viqro Dini <80572391+ivonviqrodini@users.noreply.github.com>
2021-03-17 22:00:37 -0700

    Add files via upload

---
+++ GalToLit.java
@@ -0,0 +1,13 @@
+public class GalToLit {
+    public static void main(String args[]){
+
+        double gallons;
+        double liters;
+
+        gallons = 10;
+        liters = gallons * 3.7854;
+        //convert to litters
+
+        System.out.println(gallons + "gallons is" + liters + "liters");
+    }
+}
